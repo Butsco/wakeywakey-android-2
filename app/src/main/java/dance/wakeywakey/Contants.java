73bed63ea0f29c066d5cbc4fd9d9b71aa2f532da
Bert Wijnants <bert.wijnants@gmail.com>
2014-10-18 20:30:30 +0100

    fancy datepicker FTW

---
+++ app/src/main/java/dance/wakeywakey/Contants.java
@@ -0,0 +1,9 @@
+package dance.wakeywakey;
+
+/**
+ * Created by bert on 18/10/14.
+ */
+public class Contants {
+
+    public static final String TAG = "WakeyWakey";
+}
--- app/src/main/java/dance/wakeywakey/SetAlarmFragment.java
+++ app/src/main/java/dance/wakeywakey/SetAlarmFragment.java
@@ -6,14 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
 
 /**
  * Created by bert on 18/10/14.
  */
-public class SetAlarmFragment extends Fragment {
-
-    TimePicker timePicker;
+public class SetAlarmFragment extends Fragment implements TimePickerDialog.OnTimeSetListener {
+    private TextView timePickerTextView;
+    private int alarmHour = 8;
+    private int alarmMinutes = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,10 +33,31 @@ public class SetAlarmFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        timePicker = (TimePicker) getView().findViewById(R.id.time_picker);
-        timePicker.setIs24HourView(true);
-        timePicker.setCurrentHour(8);
-        timePicker.setCurrentMinute(0);
+        final Calendar calendar = Calendar.getInstance();
+        final TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY) ,calendar.get(Calendar.MINUTE), false, false);
 
+        timePickerTextView = (TextView) getView().findViewById(R.id.time_picker);
+        timePickerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.setCloseOnSingleTapMinute(false);
+                timePickerDialog.initialize(SetAlarmFragment.this, alarmHour, alarmMinutes, true, false);
+                timePickerDialog.show(getActivity().getSupportFragmentManager(), Contants.TAG);
+            }
+        });
+
+        setAlarmTime(alarmHour, alarmMinutes);
+    }
+
+    private void setAlarmTime(int hour, int minutes) {
+        alarmHour = hour;
+        alarmMinutes = minutes;
+
+        timePickerTextView.setText(String.format("%d:%02d", alarmHour, alarmMinutes));
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
+        setAlarmTime(hourOfDay, minute);
     }
 }
